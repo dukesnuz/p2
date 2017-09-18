d9637717948243ea688864fdba4e9537d59b6037
David Petringa <david@ajaxtransport.com>
2017-09-18 17:45:57 -0400

    Added save entered input values, and check all fields have a value

--- logic/p2Logic.php
+++ logic/p2Logic.php
@@ -1,42 +1,69 @@
 <?php
-require ('../helpers.php');
+//require ('../helpers.php');
 $dishesJson = file_get_contents('./data/menu.json');
 
 $dishes = json_decode($dishesJson, true);
 
-$output='';
+// Set variables to not cause errors
+$dishName='';
+$dishNutrition='';
 $maxCalories='';
 $nutrition='';
 $protein='';
 $outputHeading ='';
 $errorProtein = 'Optional';
-//check that form is filled in
+$diet='';
+$nonDiet='';
+$beef='';
+$chicken='';
+$eggs='';
+$pork='';
 
+// check that form is filled in
 if(empty($_GET['maxCalories']) || !filter_var($_GET['maxCalories'], FILTER_VALIDATE_INT)) {
   $errorCalories = 'Required. Please enter only numbers';
 } else {
    $errorCalories = '';
+   $maxCalories=$_GET['maxCalories'];
 }
 
 if(!isset($_GET['nutrition'])) {
   $errorNutrition = 'Required. Please select Diet or Non Diet.';
 } else {
    $errorNutrition = '';
+   $nutrition=$_GET['nutrition'];
+}
+
+if (isset($_GET['protein'])){
+	$protein=$_GET['protein'];
+}
+// Save entered values to form
+if($nutrition=='diet') {
+    $diet = 'CHECKED';
+}
+if($nutrition=='nonDiet') {
+	$nonDiet = 'CHECKED';
+}
+if ($protein =='beef') {
+	$beef= 'SELECTED';
+}
+if ($protein =='chicken') {
+	$chicken= 'SELECTED';
+}
+if ($protein =='eggs') {
+	$eggs= 'SELECTED';
+}
+if ($protein =='pork') {
+	$pork= 'SELECTED';
 }
 
 // Set variables to an empty string
-$dishName=$dishNutrition=$dishApetizer=$dishEntree=$dishDesert=$dishCalories='';
+//$dishName=$dishNutrition=$dishApetizer=$dishEntree=$dishDesert=$dishCalories='';
 
-if (isset($_GET['maxCalories']) && isset($_GET['nutrition'])) {
-	// set variables
-	$maxCalories=$_GET['maxCalories'];
-	$nutrition=$_GET['nutrition'];
-	$protein=$_GET['protein'];
-
-      for ($i=0;  $i < count($dishes); $i++) {
+    //  for ($i=0;  $i < count($dishes); $i++) {
 		  //echo $dishes["No Limit Ribs"]['nutrition'];
 
-	  }
+	//  }
 
 	//foreach ($dishes as $key => $dish) {
 		// set the variables
@@ -44,7 +71,7 @@ if (isset($_GET['maxCalories']) && isset($_GET['nutrition'])) {
 	   // $dishNutrition=$dish['nutrition'];
 		// check if a match
 	//	if ($dishCalories <= $maxCalories && $nutrition == $dishNutrition) {
-	        unset($dishes[$key]); // used in video use another code style
+	  //      unset($dishes[$key]); // used in video use another code style
 			//$dishName.=$key;
 			//$dishNutrition.=$dish['nutrition'];
 	       // $dishApetizer.=$dish['appetizer'];
@@ -53,7 +80,7 @@ if (isset($_GET['maxCalories']) && isset($_GET['nutrition'])) {
 	       //$dishCalories=$dish['calories'];
 		//}
 //	}
-   $outputHeading='You selected the folowing';
-} else {
-	$output = '';
-}
+ //  $outputHeading='You selected the folowing';
+//} else {
+	//$outputHeading = '';
+//}
