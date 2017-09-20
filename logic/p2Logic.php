6226c42dd83d036ae97501e186b078b6ed049df6
David Petringa <david@ajaxtransport.com>
2017-09-20 13:48:09 -0400

    swapped v1 to v2 and v2 to v1, added protein to logic

--- logic/p2Logic.php
+++ logic/p2Logic.php
@@ -1,12 +1,12 @@
 <?php
-/*======================================================
+/*=================================================
 // script - p2Logic.php
 // Class - HES - Dynamic Web Applications - Project 2 - Fall 2017
 // Student: David Petringa
 // Susan, Thank you for checking my work.
 // This app will pick a dinner menu according to the user selections.
-// version 1 most php logic in display
-========================================================*/
+// Version 1 has more php logic in the logic script
+================================================*/
 require ('../helpers.php');
 $dishesJson = file_get_contents('./data/menu.json');
 
@@ -15,51 +15,65 @@ $dishes = json_decode($dishesJson, true);
 // Set variables in order to not cause errors
 $maxCalories='';
 $nutrition='';
-$errorProtein = 'Optional';
 $diet='';
 $nonDiet='';
 $protein='';
 $beef='';
 $chicken='';
 $eggs='';
+$fish='';
 $pork='';
 $outputClass='outputHide';
 
 // Check that form is filled in
 if(empty($_GET['maxCalories']) || !filter_var($_GET['maxCalories'], FILTER_VALIDATE_INT)) {
-  $errorCalories = 'Required. Please enter only numbers';
+    $errorCalories = 'Required. Please enter only numbers';
 } else {
-   $errorCalories = '';
-   $maxCalories=sanitize($_GET['maxCalories']);
+    $errorCalories = '';
+    $maxCalories=sanitize($_GET['maxCalories']);
 }
 
 if(!isset($_GET['nutrition'])) {
-  $errorNutrition = 'Required. Please select Diet or Non Diet.';
+    $errorNutrition = 'Required. Please select Diet or Non Diet.';
 } else {
-   $errorNutrition = '';
-   $nutrition=sanitize($_GET['nutrition']);
-   $outputClass='outputDisplay';
+    $errorNutrition = '';
+    $nutrition=sanitize($_GET['nutrition']);
+    $outputClass='outputDisplay';
 }
 
-if (isset($_GET['protein'])){
-	$protein=sanitize($_GET['protein']);
+if (isset($_GET['protein']) && $_GET['protein'] !='select'){
+    $protein=sanitize($_GET['protein']);
+    $errorProtein ='';
+} else {
+    $errorProtein='Required. Please select a protein.';
 }
 // Save entered values to form
 if($nutrition=='diet') {
     $diet = 'CHECKED';
 }
 if($nutrition=='nonDiet') {
-	$nonDiet = 'CHECKED';
+    $nonDiet = 'CHECKED';
 }
 if ($protein =='beef') {
-	$beef= 'SELECTED';
+    $beef= 'SELECTED';
 }
 if ($protein =='chicken') {
-	$chicken= 'SELECTED';
+    $chicken= 'SELECTED';
 }
 if ($protein =='eggs') {
-	$eggs= 'SELECTED';
+    $eggs= 'SELECTED';
+}
+if ($protein =='fish') {
+    $fish= 'SELECTED';
 }
 if ($protein =='pork') {
-	$pork= 'SELECTED';
+    $pork= 'SELECTED';
+}
+
+// Add matched dishes to an array
+$foundDishes = [];
+foreach ($dishes as $key => $dish) {
+	if ($dishes[$key]['calories'] <= $maxCalories && $nutrition == $dishes[$key]['nutrition'] && $protein == $dishes[$key]['protein']) {
+		$foundDishes[$key] = $dish;
+	}
 }
