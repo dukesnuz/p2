c0f3dd4ff8713f9c8b1578858e9174b7e467e83e
David Petringa <david@ajaxtransport.com>
2017-09-27 09:00:34 -0400

    Progress Log, Email class

---
+++ libraries/Email.php
@@ -0,0 +1,17 @@
+<?php
+namespace DWA;
+
+class Email {
+	// Properties
+	private $recipient;
+
+    // Methods
+	public function __contruct($recipient){
+		$this->recipient = $recipient;
+	}
+
+	public function send($subject, $message){
+		return mail($this->recipient, $subject, $message);
+	}
+
+} //END class
